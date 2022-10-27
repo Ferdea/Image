d6e272a02a930523423061b290a4f132711cdb8e
Саша Сапегин <sapegin04@mail.ru>
2022-10-27 23:57:42 +0500

    Done GrayscaleTask!

--- GrayscaleTask.cs
+++ GrayscaleTask.cs
@@ -1,26 +1,22 @@
 ﻿namespace Recognizer
 {
-	public static class GrayscaleTask
-	{
-		/* 
-		 * Переведите изображение в серую гамму.
-		 * 
-		 * original[x, y] - массив пикселей с координатами x, y. 
-		 * Каждый канал R,G,B лежит в диапазоне от 0 до 255.
-		 * 
-		 * Получившийся массив должен иметь те же размеры, 
-		 * grayscale[x, y] - яркость пикселя (x,y) в диапазоне от 0.0 до 1.0
-		 *
-		 * Используйте формулу:
-		 * Яркость = (0.299*R + 0.587*G + 0.114*B) / 255
-		 * 
-		 * Почему формула именно такая — читайте в википедии 
-		 * http://ru.wikipedia.org/wiki/Оттенки_серого
-		 */
+    public static class GrayscaleTask
+    {
+        public static double[,] ToGrayscale(Pixel[,] original)
+        {
+            var originalWidth = original.GetLength(0);
+            var originalHeight = original.GetLength(1);
+            var result = new double[originalWidth, originalHeight];
+            for (var i = 0; i < originalWidth; i++)
+            {
+                for (var j = 0; j < originalHeight; j++)
+                {
+                    var pixel = original[i, j];
+                    result[i, j] = (0.299 * pixel.R + 0.587 * pixel.G + 0.114 * pixel.B) / 255;
+                }
+            }
 
-		public static double[,] ToGrayscale(Pixel[,] original)
-		{
-			return new double[original.GetLength(0), original.GetLength(1)];
-		}
-	}
+            return result;
+        }
+    }
 }
