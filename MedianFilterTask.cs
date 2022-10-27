9e5a308b29f21cfd2efa85dde0e2b4c1b238720c
Саша Сапегин <sapegin04@mail.ru>
2022-10-27 23:58:16 +0500

    Done MedianFilterTask!

--- MedianFilterTask.cs
+++ MedianFilterTask.cs
@@ -1,21 +1,39 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Recognizer
 {
-	internal static class MedianFilterTask
-	{
-		/* 
-		 * Для борьбы с пиксельным шумом, подобным тому, что на изображении,
-		 * обычно применяют медианный фильтр, в котором цвет каждого пикселя, 
-		 * заменяется на медиану всех цветов в некоторой окрестности пикселя.
-		 * https://en.wikipedia.org/wiki/Median_filter
-		 * 
-		 * Используйте окно размером 3х3 для не граничных пикселей,
-		 * Окно размером 2х2 для угловых и 3х2 или 2х3 для граничных.
-		 */
-		public static double[,] MedianFilter(double[,] original)
-		{
-			return original;
-		}
-	}
+    internal static class MedianFilterTask
+    {
+        private static double GetMedian(List<double> neighborhood)
+        {
+            var count = neighborhood.Count;
+            neighborhood.Sort();
+            if (count % 2 == 0)
+                return (neighborhood[count / 2 - 1] + neighborhood[count / 2]) / 2;
+            return neighborhood[count / 2];
+        }
+		
+        public static double[,] MedianFilter(double[,] original)
+        {
+            var originalWidth = original.GetLength(0);
+            var originalHeight = original.GetLength(1);
+            var result = new double[originalWidth, originalHeight];
+            var neighborhood = new List<double>();
+            for (var i = 0; i < originalWidth; i++)
+            for (var j = 0; j < originalHeight; j++)
+            {
+                for (var x = Math.Max(i - 1, 0); x < Math.Min(i + 2, originalWidth); x++)
+                for (var y = Math.Max(j - 1, 0); y < Math.Min(j + 2, originalHeight); y++)
+                {
+                    neighborhood.Add(original[x, y]);
+                }
+
+                result[i, j] = GetMedian(neighborhood);
+                neighborhood.Clear();
+            }
+            return result;
+        }
+    }
 }
