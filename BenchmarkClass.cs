9d4069605b74d69641e8c1fc9f6bc68a07798b2f
Ferdea <sapegin04@mail.ru>
2022-11-01 10:05:00 +0300

    BenchmarkTask Init

---
+++ BenchmarkClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Recognizer
+{
+    public class BenchmarkClass
+    {
+        public static void SomeMethodVer1(double[,] image)
+        {
+            image.GetLength(0);
+            image.GetLength(1);
+            for (var i = 0; i < 10000; i++)
+            for (var j = 0; j < 10000; j++)
+            {
+                
+            }
+        }
+        
+        public static void SomeMethodVer2(double[,] image)
+        {
+            for (var i = 0; i < 10000; i++)
+            for (var j = 0; j < 10000; j++)
+            {
+                image.GetLength(0);
+                image.GetLength(1);
+            }
+        }
+
+        private static int GetLength(double[,] image, int index)
+        {
+            return image.GetLength(index);
+        }
+
+        public static void SomeMethodVer3(double[,] image)
+        {
+            GetLength(image, 0);
+            GetLength(image, 1);
+            for (var i = 0; i < 10000; i++)
+            for (var j = 0; j < 10000; j++)
+            {
+                
+            }
+        }
+    }
+}
---
+++ BenchmarkClassTest.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using NUnit.Framework;
+using Recognizer;
+
+namespace Recognizer
+{
+    public class BenchmarkClassTest
+    {
+        [Benchmark(Description = "1")]
+        public void Test1()
+        {
+            BenchmarkClass.SomeMethodVer1(new double[100,100]);
+        }
+        
+        [Benchmark(Description = "2")]
+        public void Test2()
+        {
+            BenchmarkClass.SomeMethodVer2(new double[100,100]);
+        }
+        
+        [Benchmark(Description = "3")]
+        public void Test3()
+        {
+            BenchmarkClass.SomeMethodVer3(new double[100,100]);
+        }
+    }
+
+    [TestFixture]
+    class UnitTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            var config = new ManualConfig()
+                .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+                .AddValidator(JitOptimizationsValidator.DontFailOnError)
+                .AddLogger(ConsoleLogger.Default)
+                .AddColumnProvider(DefaultColumnProviders.Instance);
+
+            BenchmarkRunner.Run<BenchmarkClassTest>(config);
+        }
+    }
+}
